f4920f50c7b4786ad1489b8363b94d89e4322d8c
Naoki Otani <notani@cs.cmu.edu>
2018-03-29 09:29:36 -0400

    Added GErnam, Russian, and Chinese data of the 2018-03-20 dumps

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
 DATE=$1
 
-for lang in en es fr it
+for lang in de en es fr it ru zh
 do
     if [ -e results/${lang}wiki-${DATE}-words-frequency.txt ]; then
         continue
     fi
 
     # 1. Download
-    if [ ! -d https://dumps.wikimedia.org/${lang}wiki/${DATE}/ ]; then
-        echo "wget -np -r --accept-regex 'https://dumps.wikimedia.org/${lang}wiki/${DATE}/${lang}wiki-${DATE}-pages-articles[0-9].*' https://dumps.wikimedia.org/${lang}wiki/${DATE}/"
+    if [ ! -d dumps.wikimedia.org/${lang}wiki/${DATE}/ ]; then
+        cmd="wget -np -r --accept-regex 'https://dumps.wikimedia.org/${lang}wiki/${DATE}/${lang}wiki-${DATE}-pages-articles[0-9].*' https://dumps.wikimedia.org/${lang}wiki/${DATE}/"
+        echo $cmd
+        eval $cmd
     fi
 
     # 2. Count
-    echo "python ./gather_wordfreq.py dumps.wikimedia.org/${lang}wiki/${DATE}/*.bz2 > results/${lang}wiki-${DATE}-words-frequency.txt"
+    cmd="python ./gather_wordfreq.py dumps.wikimedia.org/${lang}wiki/${DATE}/*.bz2 > results/${lang}wiki-${DATE}-words-frequency.txt"
+    echo $cmd
+    eval $cmd
 done
